d38faac5880fdb7efc118c7e11a741d0447aec7c
fayzannnn <babushikalgar70@gmail.com>
2026-08-09 02:10:41 +0530

    url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://fitness-club-backend.vercel.app/'
+  baseURL: process.env.REACT_APP_API_URL || 'https://fitness-backend-u6gw.onrender.com'
 });
 API.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token');
